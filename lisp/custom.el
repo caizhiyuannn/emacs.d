60bc63fcb4371f170753f09a647b558cfb207e0a
caizhiyuannn <caizhiyuannn@gmail.com>
2018-10-15 01:42:42 +0800

    添加依赖包内容

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(exec-path-from-shell-check-startup-files nil)
  '(package-selected-packages
    (quote
-    (htmlize ox-jekyll yaml-mode web-mode exec-path-from-shell switch-window evil spaceline-all-the-icons helm material-theme space-line-all-the-icons all-the-icons neotree hungry-delete lsp-ui company-lsp lsp-python lsp-mode company use-package smartparens helm-directory))))
+    (htmlize ox-jekyll yaml-mode web-mode exec-path-from-shell switch-window evil helm material-theme powerline spaceline all-the-icons neotree hungry-delete lsp-ui company-lsp lsp-python lsp-mode company use-package smartparens helm-directory))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
